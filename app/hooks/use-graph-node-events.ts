5a025b42c8887ac9ef4610e58e076c5c940685b2
Mohamad Jawhar <mohamad.jaouhar98@gmail.com>
2024-07-21 21:27:56 -0400

    add typography mdx and update graph node events

--- app/hooks/use-graph-node-events.ts
+++ app/hooks/use-graph-node-events.ts
@@ -2,7 +2,11 @@ import type { KonvaEventObject } from 'konva/lib/Node'
 import { useCallback } from 'react'
 
 export const useGraphNodeEvents = () => {
-  const onMouseEnter = useCallback(() => {
+  const onMouseOver = useCallback(() => {
+    document.body.style.cursor = 'grab'
+  }, [])
+
+  const onMouseMove = useCallback(() => {
     document.body.style.cursor = 'grab'
   }, [])
 
@@ -15,10 +19,10 @@ export const useGraphNodeEvents = () => {
     e.target.to({ scaleX: 1.2, scaleY: 1.2, duration: 0.05 })
   }, [])
 
-  const onMouseUp = useCallback((e: KonvaEventObject<DragEvent>) => {
-    document.body.style.cursor = 'grab'
+  const onDragEnd = useCallback((e: KonvaEventObject<DragEvent>) => {
+    document.body.style.cursor = 'default'
     e.target.to({ scaleX: 1, scaleY: 1, duration: 0.05 })
   }, [])
 
-  return { onMouseEnter, onMouseLeave, onMouseDown, onMouseUp }
+  return { onMouseOver, onMouseMove, onMouseLeave, onMouseDown, onDragEnd }
 }
